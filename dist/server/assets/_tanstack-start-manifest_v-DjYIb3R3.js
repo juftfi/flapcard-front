e8ce7564ee29f30026a383c715c96ac93852bf4b
juftfi <107974770+juftfi@users.noreply.github.com>
2026-05-11 19:47:25 +0700

    first

---
+++ dist/server/assets/_tanstack-start-manifest_v-DjYIb3R3.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "E:/AI-Agent/Agents/FlapCard/flapcard/flapcard-front/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-B8yoRhMb.js"] }, "/": { filePath: "E:/AI-Agent/Agents/FlapCard/flapcard/flapcard-front/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-zRHd2cfi.js"] } }, clientEntry: "/assets/index-B8yoRhMb.js" });
+export {
+  tsrStartManifest
+};
---
+++ dist/server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-DplRN7oB.js";
+import "node:events";
+export {
+  w as default
+};
